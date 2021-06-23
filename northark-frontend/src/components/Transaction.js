25c7e13526194ce7334792089cc1de2313590faa
mikeisfake <mikescooper@icloud.com>
2021-06-23 15:06:51 -0400

    stub out a few components

---
+++ northark-frontend/src/components/Transaction.js
@@ -0,0 +1,7 @@
+export const Transaction = props => {
+  return (
+    // not sure how to format these but I hate working with tables so maybe a list??
+    // this would be the component for the individual transaction item. Ideally pull transactions from DB and map over them to make this compoennt for each one and pass data as props to this.
+    <li className="transaction-item"></li>
+  )
+}
