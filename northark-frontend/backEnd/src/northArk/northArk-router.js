b7e371a439744fd8b26f43c9d9b7e7055e3a2a09
yl1462 <yliang1462@hotmail.com>
2021-06-30 15:29:34 -0500

    basic GET route

--- northark-frontend/backEnd/src/northArk/northArk-router.js
+++ northark-frontend/backEnd/src/northArk/northArk-router.js
@@ -1,36 +1,47 @@
 const express = require('express'),
   path = require('path')
-xss = require('xss'),
+  xss = require('xss'),
   northArkService = require('./northArk-service'),
   northArkRouter = express.Router(),
   jsonParser = express.json();
 
+//this is a general idea of how to get user by id, need help with connecting the tables and return data (account, account_type, balance...etc.)
+
+const serializeUser = user => ({
+  id: user.id,
+  the_user: xss(user.the_user),
+  type: xss(user.type)
+});
+
 northArkRouter
   .route('/')
   .get((req, res, next) => {
-
+    const knexInstance = req.app.get('db');
+    northArkService.getUserById(knexInstance)
+      .then(user => {
+        res.json(user.map(serializeUser));
+      })
+      .catch(next);
   })
-  .post(jsonParser, (req, res, next) => {
-
-  });
 
 northArkRouter
-  .route('/:')
-
+  .route('/:userid')
   .all((req, res, next) => {
-
+    northArkService.getById(req.app.get('db'), req.params.userid)
+      .then(user => {
+        if (!user) {
+          return res.status(404).json({
+            error: { message: 'No matching user found' }
+          });
+        }
+        res.user = user;
+        next();
+      })
+      .catch(next);
   })
 
   .get((req, res, next) => {
-
-  })
-
-  .patch(jsonParser, (req, res, next) => {
-
-  })
-
-  .delete((req, res, next) => {
-
+    res.json(serializeUser(res.user));
   })
 
 module.exports = northArkRouter
--- northark-frontend/backEnd/src/northArk/northArk-service.js
+++ northark-frontend/backEnd/src/northArk/northArk-service.js
@@ -2,16 +2,6 @@ const northArkService = {
   getAll(knex) {
     return knex.select('*').from('northArk');
   },
-  //add new restaurant
-  insert(knex) {
-    return knex
-      .insert()
-      .into('northArk')
-      .returning('*')
-      .then(rows => {
-        return rows[0];
-      });
-  },
 
   getById(knex, id) {
     return knex
@@ -20,17 +10,6 @@ const northArkService = {
       .where({ id })
       .first();
   },
-
-  delete(knex, id) {
-    console.log(id)
-    return knex('northArk').where({ id }).delete();
-  },
-
-  edit(knex, id, edited) {
-    return knex('northArk')
-      .where({ id })
-      .update(edited);
-  }
 };
 
 module.exports = northArkService;
