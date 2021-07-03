e6b602ec56cad41d3f6cf4327488629547f89076
Cesar Nina <c3sar207@gmail.com>
2021-07-03 09:56:26 -0400

    created a Table component for the transaction

--- northark-frontend/src/index.js
+++ northark-frontend/src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import Layout from './components/Layout';
+import Table from './components/Table';
 
 
 ReactDOM.render(
   <React.StrictMode>
     <Layout>
-      <App />
+      {/* <App /> */}
+      <Table />
     </Layout>
   </React.StrictMode>,
   document.getElementById('root')
