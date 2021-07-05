1f0fbba6d933cf5d722d01c5b8715ee2c6b56dc4
JGunnFo <jeffgunn814@gmail.com>
2021-07-05 16:06:28 -0700

    Adjusted comments in order to give clear direction for next tasks, removed unnecessary comments.

--- northark-frontend/src/components/Transaction.js
+++ northark-frontend/src/components/Transaction.js
@@ -3,16 +3,25 @@ import { UserContext } from '../contexts/UserContext'
 import React, { useEffect, useState } from 'react';
 
   /*
-  Todo for fetch and the useeffect for it:
-  May need to adjust the fetch based on what the backend API expects. I tried to match it to the latest backend files.
-  Useeffect will probably need context.userID passed as the first argument. Cesar can add that once context is implemented.
+Todo for Yuri/Xiang:
+Be aware: Transaction component is for Cesar to handle. Currently Transaction is not rendered, 
+instead the Table component in Table.js(with fake data supplied) is being directly rendered at the moment, which will change.
 
-  
-  Sort switch is just an example for now.
+-Change the fetch URL to the heroku URL.
+-Adjust how queries are added to the url, based on argument. 
+-Adjust any remaining json data parsing needed.
+-Add error catching, return error message if not 200.
+-For now, no sorting. I just left example code in case.
+
+
   */
 
 
   export const Transaction = props => {
+
+    /*
+    Context API(ie, user.id below) worked last time Jeffrey checked it(july 5, 4pm PST). But double check if things change.
+    */
 
 
     let [transactionsData, setTransactionsData] = useState("")
@@ -23,10 +32,8 @@ import React, { useEffect, useState } from 'react';
       .then(value=>setTransactionsData((value)))
   
       /*
-      Simple example arguments for now. 1 and "default" correspond to account number and sort type.
-      Once the parent component wraps this component in context and the context is imported here,
-      you would probably put something like context.userID for the userID argument of fetchTransactions. 
-  
+      Simple example arguments for now. 1 and "default" correspond to account number and sort type.  
+      Needs error handling.
   
       Then in the render, 
       you would map over {transactionsData}, probably. Or convert it in some way, if needed.
@@ -45,7 +52,7 @@ import React, { useEffect, useState } from 'react';
     return (
       // not sure how to format these but I hate working with tables so maybe a list??
       // this would be the component for the individual transaction item. Ideally pull transactions from DB and map over them to make this compoennt for each one and pass data as props to this.
-      <li className="transaction-item">temporary text</li>
+      <li className="transaction-item">temporary text{user.id}</li>
     )
   }
 
@@ -56,7 +63,7 @@ async function fetchTransactions(userID, account, sort){
 
 
   /*
-  This is an example for how the search function may work in the future. 
+  This is an example for how the sort function may work in the future. 
   Unused for now.
   If we do this, will have to decide the sorts, their string names, make sure we have the API commands right, etc.
   The sort should probably be placed in a separate function, leaving here as a quick example
@@ -77,18 +84,15 @@ async function fetchTransactions(userID, account, sort){
       break;
   }
 
-  /*
-  Not tested, and backend is apparently in flux, so you may need to talk to Yuri/Xiang about proper syntax.
-  May have to convert the userID prop to a string or something.
-  The port may not be correct.
-  */
+
   let response = await fetch('https://localhost:8000/'+userID)
 
 
   let result = await response.json();
 
+
   /*
-  Returning a specific account of that userID.
+  Returning a specific account of that userID, so that the tables can show that account's data.
   */
   return result.account;
 
